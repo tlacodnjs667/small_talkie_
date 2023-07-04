0880b07c6af1f898cbb62bc8d7887f5dba878995
wooruk <114975208+tlacodnjs667@users.noreply.github.com>
2023-07-04 23:03:16 +0900

    :card_file_box: Fix : migration 수정
    직전 migration 오류 수정

--- db/migrations/20230704110752_alterEncounterCategoryTbl.sql
+++ db/migrations/20230704110752_alterEncounterCategoryTbl.sql
@@ -1,5 +1,6 @@
 -- migrate:up
 ALTER TABLE encounter_category 
+ADD COLUMN situation_id INT NOT NULL,
 ADD FOREIGN KEY(situation_id) REFERENCES situation_category(id) ON DELETE CASCADE;
 
 -- migrate:down
